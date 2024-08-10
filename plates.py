d1b3585c91d6e6ab63140aff3f95a69a40580105
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-10 21:41:56 +0630

    Add check if first number is 0

--- plates.py
+++ plates.py
@@ -5,7 +5,15 @@ def main():
 
 def is_valid(s):
     if check_first(s) and check_length(s) and check_end(s):
-        ...
+        for i in range(len(s)):
+            if s[i] == '0' and s[i - 1].isalpha():
+                break
+            
+        return True
+
+    else:
+        return False
+
 
 def check_first(s):
     return True if s[0:2].isalpha() else False 
