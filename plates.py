5389131fc1d6938d3843ec857290cc6976e74bbb
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-11 16:46:26 +0630

    Fix punctuation bug

--- plates.py
+++ plates.py
@@ -8,7 +8,7 @@ def main():
 
 
 def is_valid(s):
-    if check_first(s) and check_length(s):
+    if check_first(s) and check_length(s) and check_punct(s):
         for i in range(len(s)):
             if (s[i] == "0" and s[i - 1].isalpha()) or (
                 s[i].isnumeric() and s[i] != s[-1] and s[i + 1].isalpha()
