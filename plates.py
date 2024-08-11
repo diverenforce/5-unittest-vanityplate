9dca0d34b549caf799036d2a1d3019d5bcf842f6
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-11 16:31:04 +0630

    Fix bugs and finalize code

--- plates.py
+++ plates.py
@@ -4,15 +4,16 @@ def main():
     if is_valid(plate):
         print("Valid")
     else:
-        print("invalid")
+        print("Invalid")
 
 
 def is_valid(s):
-    if check_first(s) and check_length(s) and check_end(s):
+    if check_first(s) and check_length(s):
         for i in range(len(s)):
-            if (s[i] == '0' and s[i - 1].isalpha()) or (s[i].isnumeric() and s[i + 1].isnumeric()):
-                break
-            
+            if (s[i] == "0" and s[i - 1].isalpha()) or (
+                s[i].isnumeric() and s[i] != s[-1] and s[i + 1].isalpha()
+            ):
+                return False
 
         return True
 
@@ -21,7 +22,8 @@ def is_valid(s):
 
 
 def check_first(s):
-    return True if s[0:2].isalpha() else False 
+    return True if s[0:2].isalpha() else False
+
 
 def check_length(s):
     if 2 < len(s) <= 6:
@@ -29,10 +31,10 @@ def check_length(s):
     else:
         return False
 
-def check_end(s):
-    return True if s[-1].isnumeric() else False
 
-    
+# def check_end(s):
+#     return True if s[-1].isnumeric() else False
+
 
 if __name__ == "__main__":
     main()
